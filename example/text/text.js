ca2ef8ff475189e2311acde4f53a884e7e610f75
Mgen <m@mgenware.com>
2018-06-10 02:29:27 +0800

    Use a dedicated js file in example

---
+++ example/text/text.js
@@ -0,0 +1,30 @@
+let playground = undefined;
+let textElement = undefined;
+
+function checkEnv() {
+  const cfn = window.cfn;
+  if (!cfn) {
+    throw new Error('cf-neptune not found, please build the project and try again');
+  }
+}
+
+function longStringClick() {
+  textElement.text = 'This is a looooooooooooooooong string 🦁🦁🦁';
+}
+
+function shortStringClick() {
+  textElement.text = 'A';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  checkEnv();
+  
+  const cfn = window.cfn;
+  // Create the text element
+  textElement = new cfn.Text('cf-neptune');
+  const root = document.getElementById('playground');
+
+  // Mount the text element to playground
+  playground = cfn.Playground.create(root, {width: 300, height: 100}, textElement);
+  playground.layout();
+});
