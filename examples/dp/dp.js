43876e7cca0b18c794581a373ec6c91a73f2f8be
Mgen <m@mgenware.com>
2018-07-16 02:45:23 +0800

    Update dp example

--- examples/dp/dp.js
+++ examples/dp/dp.js
@@ -3,6 +3,14 @@ const SIZE = { width: 500, height: 500 };
 const ROWS = 5;
 const COLS = 5;
 
+// Obstacle (-1) should be counted as 0 possible path.
+function getCellValue(value) {
+  if (value <= 0) {
+    return 0;
+  }
+  return value;
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   window.checkEnv();
   
@@ -13,16 +21,43 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   matrix.fill(() => 0);
 
+  // Set obstacles
   const obstacles = [
-    [2, 2],
+    [1, 2],
     [3, 2],
     [4, 3],
   ];
-
   for (const obstacle of obstacles) {
-    const cell = matrix.row(obstacle[0]).child(obstacle[1]);
+    const cell = matrix.cell(obstacle[0], obstacle[1]);
     cell.content = -1;
     cell.backgroundColor = 'pink';
+  }
+
+  // Set boundary rows color
+  for (let i = 0; i <= ROWS; i++) {
+    matrix.cell(i, COLS).backgroundColor = 'gray';
+  }
+  for (let i = 0; i <= COLS; i++) {
+    matrix.cell(ROWS, i).backgroundColor = 'gray';
+  }
+
+  // Set destination to 1
+  const destCell = matrix.cell(ROWS - 1, COLS - 1);
+  destCell.content = 1;
+  destCell.backgroundColor = 'red';
+
+  matrix.cell(ROWS, COLS - 1).content = 1;
+  for (let i = ROWS - 1; i >= 0; i--) {
+    await matrix.set2DPointerAsync(i, 0, 'i');
+    for (let j = COLS - 1; j >= 0; j--) {
+      await matrix.set2DPointerAsync(i, j, 'j');
+
+      const cell = matrix.cell(i, j);
+      if (cell.content !== -1) {
+        const value = getCellValue(matrix.cellContent(i, j + 1)) + getCellValue(matrix.cellContent(i + 1, j));
+        cell.setContentAsync(value);
+      }
+    }
   }
 
   matrixElement = matrix;
