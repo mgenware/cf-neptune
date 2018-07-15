0b0e606db0dd92bdcceb5fa2c970f1ed72ed7e6f
Mgen <m@mgenware.com>
2018-07-15 18:40:44 +0800

    Update dp example

--- examples/dp/dp.js
+++ examples/dp/dp.js
@@ -8,10 +8,22 @@ document.addEventListener('DOMContentLoaded', async () => {
   
   const nep = window.nep;
   const root = document.getElementById('playground');
-  const matrix = new nep.Matrix(SIZE, ROWS, COLS);
+  const matrix = new nep.Matrix(SIZE, ROWS + 1, COLS + 1);
   nep.newPlayground(root, matrix);
 
-  await matrix.set2DPointerAsync(1, 4, 'i');
+  matrix.fill(() => 0);
+
+  const obstacles = [
+    [2, 2],
+    [3, 2],
+    [4, 3],
+  ];
+
+  for (const obstacle of obstacles) {
+    const cell = matrix.row(obstacle[0]).child(obstacle[1]);
+    cell.content = -1;
+    cell.backgroundColor = 'pink';
+  }
 
   matrixElement = matrix;
 });
