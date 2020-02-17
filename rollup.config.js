020e3c0cccfc8070a4ced86fb0b8f562b93a7074
Mgen <m@mgenware.com>
2020-02-17 23:59:26 +0800

    Rename lib to src

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import { uglify } from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es';
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
+import { minify } from "uglify-es";
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
 export default {
-  input: 'lib/main.ts',
+  input: "src/main.ts",
   output: {
-    name: 'nep',
-    file: 'dist/main.js',
-    format: 'umd',
+    name: "nep",
+    file: "dist/main.js",
+    format: "umd"
   },
   plugins: [
     resolve({
       module: true,
-      browser: true,
+      browser: true
     }),
     commonjs(),
-    typescript({ cacheRoot: (require('unique-temp-dir'))() }),
+    typescript({ cacheRoot: require("unique-temp-dir")() }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
-    production && uglify({}, minify),
-  ],
+    production && uglify({}, minify)
+  ]
 };
